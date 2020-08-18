6a00b6371f7cdf4eeb2d6c410894c4d762c06271
Mocsonoky Attila <kismocsy@gmail.com>
2020-08-18 03:13:53 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -26,8 +26,7 @@ export default new Router({
     path: '/recipes',
     name: 'recipe',
     component: Recipe
-  },
-  , {
+  }, {
     path: '/recipe/:author',
     name: 'author',
     props: true,
