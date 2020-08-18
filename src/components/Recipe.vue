84a36eb35c81509fde2474d4dc96afa889ba618c
Mocsonoky Attila <kismocsy@gmail.com>
2020-08-18 13:26:38 +0200

    Update Recipe.vue

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,17 +1,14 @@
-// pages/index.vue
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        cosmic-nuxt-app
-      </h1>
-      <div v-if="loading">Loading...</div>
-      <div v-for="food in foods" :key="food.slug">
-        <h3>{{ food.title }}</h3>
-        <img alt="" :src="food.metadata.hero.imgix_url + '?w=400'"/>
-      </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Cosmic Vue App</h1>
+    <div v-if="loading">Loading...</div>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <div>{{ post.title }}</div>
+        <img alt="" :src="post.metadata.hero.imgix_url + '?w=400'"/>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -19,53 +16,42 @@ const Cosmic = require('cosmicjs')
 const api = Cosmic()
 // Set these values, found in Bucket > Settings after logging in at https://app.cosmicjs.com/login
 const bucket = api.bucket({
-  slug: '403ce750-e0bf-11ea-a7da-416cf5f0e16c',
-  read_key: 'KUQwJBqttZxNKM4Y2iAZndrEqCTaH0g4f5Uj7gnV0Uqdwpe8bF'
+  slug: 'COSMIC_BUCKET',
+  read_key: 'COSMIC_READ_KEY'
 })
 export default {
-  data() {
+  name: 'App',
+  data () {
     return {
-      loading: true
+      loading: false,
+      posts: null
     }
   },
-  async asyncData () {
-    const data = await bucket.getObjects({
-      type: 'foods',
-      props: 'slug,title,content,metadata' // Limit the API response data by props
-    })
-    const foods = await data.objects
-    return {
-      foods,
-      loading: false
+  created () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      this.error = this.post = null
+      this.loading = true
+      bucket.getObjects({
+        type: 'posts',
+        props: 'slug,title,content,metadata' // Limit the API response data by props
+      }).then(data => {
+        const posts = data.objects
+        this.loading = false
+        this.posts = posts
+      })
     }
   }
 }
 </script>
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
 }
 </style>
